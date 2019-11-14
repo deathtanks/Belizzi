07eab3ee19f2c6afc3c6ad122951678a11a970f3
Daniil Oleshchuk <danieloleshuk@gmail.com>
2019-11-14 22:11:02 +0200

    added right categories

--- Desserts/models.py
+++ Desserts/models.py
@@ -6,4 +6,5 @@ class Dessert(models.Model):
     components = models.CharField(default='one two three four five',max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
+
 # Create your models here.
