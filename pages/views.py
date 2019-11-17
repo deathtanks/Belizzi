8ff9085137b20cf899205f0e6e937d5a6cbb2577
deathtanks <antonlypka@gmail.com>
2019-11-17 20:37:20 +0200

    home page return

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-#from Burgers.models import Burger
 
 # Create your views here.
 def homepage(request, *args, **kwargs):
 
-    way = 'media/' + str(Burger.objects.get(id=5).img)
-    return render(request, "home.html", {'image': way})
+    return render(request, "home.html", {})
 
