efbba64377fd2721131f8d3bb71a1b3714a79d07
deathtanks <33390278+deathtanks@users.noreply.github.com>
2019-11-17 16:28:08 +0200

    Delete models.py

--- Burgers/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Burger(models.Model):
-    name = models.CharField(default='tested burger',max_length=100)
-    category = models.CharField(default='Burger',max_length=100)
-    preparing_time = models.CharField(default='10-15min',max_length=100)
-    components = models.CharField(default='one two three four five',max_length=100)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-# Create your models here.
